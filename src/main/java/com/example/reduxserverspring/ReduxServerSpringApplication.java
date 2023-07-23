e15a6b8016f31ee58c8398f56bb1d82805069bed
Michael Kaaden <github@kaaden.net>
2023-07-23 16:45:02 +0200

    initial commit

---
+++ src/main/java/com/example/reduxserverspring/ReduxServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.reduxserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduxServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReduxServerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reduxserverspring/ReduxServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reduxserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReduxServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
