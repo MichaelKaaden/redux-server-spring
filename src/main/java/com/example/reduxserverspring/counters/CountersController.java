2c702bf2d5be95a12bbe982751d93d45f5e3060c
Michael Kaaden <github@kaaden.net>
2023-07-24 21:47:34 +0200

    Implement incrementing and decrementing a counter

--- src/main/java/com/example/reduxserverspring/counters/CountersController.java
+++ src/main/java/com/example/reduxserverspring/counters/CountersController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,8 +21,22 @@ public class CountersController {
 
     @GetMapping("/counters/{index}")
     public Counter getCounter(@PathVariable("index") @Min(0) int index) {
-        logger.info("index is {}", index);
+        logger.info("getting counter with index {}", index);
         final int value = countersService.getCounter(index);
+        return new Counter(index, value);
+    }
+
+    @PutMapping("/counters/{index}/decrement")
+    public Counter decrementCounter(@PathVariable("index") @Min(0) int index) {
+        logger.info("decrementing counter with index {}", index);
+        final int value = countersService.decrement(index);
+        return new Counter(index, value);
+    }
+
+    @PutMapping("/counters/{index}/increment")
+    public Counter incrementCounter(@PathVariable("index") @Min(0) int index) {
+        logger.info("incrementing counter with index {}", index);
+        final int value = countersService.increment(index);
         return new Counter(index, value);
     }
 }
