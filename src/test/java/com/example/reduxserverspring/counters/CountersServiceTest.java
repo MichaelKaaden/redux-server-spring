14295736898879435d73bff89b5bd34a470e9121
Michael Kaaden <github@kaaden.net>
2023-07-27 15:30:27 +0200

    Test concurrent access to the same counter

--- src/test/java/com/example/reduxserverspring/counters/CountersServiceTest.java
+++ src/test/java/com/example/reduxserverspring/counters/CountersServiceTest.java
@@ -3,7 +3,14 @@ package com.example.reduxserverspring.counters;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CountersServiceTest {
     CountersService countersService;
@@ -14,19 +21,26 @@ class CountersServiceTest {
     }
 
     @Test
-    void getCounter() {
+    void testGetCounter() {
         assertSame(0, countersService.getCounter(42));
     }
 
     @Test
-    void setCounter() {
+    void testGetCounters() {
+        countersService.increment(42);
+        ArrayList<Counter> expected = new ArrayList<>(List.of(new Counter(42, 1)));
+        assertEquals(expected, countersService.getCounters());
+    }
+
+    @Test
+    void testSetCounter() {
         assertSame(0, countersService.getCounter(42));
         countersService.setCounter(42, 12);
         assertSame(12, countersService.getCounter(42));
     }
 
     @Test
-    void increment() {
+    void testIncrement() {
         assertSame(0, countersService.getCounter(42));
         countersService.increment(42);
         assertSame(1, countersService.getCounter(42));
@@ -35,7 +49,7 @@ class CountersServiceTest {
     }
 
     @Test
-    void incrementByValue() {
+    void testIncrementByValue() {
         assertSame(0, countersService.getCounter(42));
         countersService.incrementByValue(42, 3);
         assertSame(3, countersService.getCounter(42));
@@ -44,7 +58,7 @@ class CountersServiceTest {
     }
 
     @Test
-    void decrement() {
+    void testDecrement() {
         assertSame(0, countersService.getCounter(42));
         countersService.decrement(42);
         assertSame(-1, countersService.getCounter(42));
@@ -53,11 +67,53 @@ class CountersServiceTest {
     }
 
     @Test
-    void decrementByValue() {
+    void testDecrementByValue() {
         assertSame(0, countersService.getCounter(42));
         countersService.decrementByValue(42, 3);
         assertSame(-3, countersService.getCounter(42));
         countersService.decrementByValue(42, 3);
         assertSame(-6, countersService.getCounter(42));
+    }
+
+    @Test
+    void testConcurrentAccessToTheSameCounter() {
+        int theCounterEveryoneWorksOn = 42;
+        int threadNum = 5;
+        int count = 0;
+        int maxRuns = 1_000;
+
+        try {
+            // Prepare to execute and store the Futures
+            ExecutorService executor = Executors.newFixedThreadPool(threadNum);
+            List<FutureTask<Integer>> taskList = new ArrayList<>();
+
+            // Start threads
+            for (int i = 0; i < threadNum; i++) {
+                FutureTask<Integer> task = new FutureTask<>(new Callable<Integer>() {
+                    @Override
+                    public Integer call() throws Exception {
+                        for (int j = 0; j < maxRuns; j++) {
+                            countersService.decrement(theCounterEveryoneWorksOn);
+                            countersService.increment(theCounterEveryoneWorksOn);
+                        }
+
+                        countersService.increment(theCounterEveryoneWorksOn);
+                        return countersService.getCounter(theCounterEveryoneWorksOn);
+                    }
+                });
+                taskList.add(task);
+                executor.execute(task);
+            }
+
+            for (int i = 0; i < threadNum; i++) {
+                FutureTask<Integer> futureTask = taskList.get(i);
+                count += futureTask.get();
+            }
+            executor.shutdown();
+        } catch (Exception e) {
+            fail(e);
+        }
+
+        assertSame(threadNum, countersService.getCounter(theCounterEveryoneWorksOn));
     }
 }
