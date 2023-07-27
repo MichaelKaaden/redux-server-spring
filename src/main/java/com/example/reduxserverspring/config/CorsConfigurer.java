c714ebbec498a680369bbb94f5557c01fffd17ac
Michael Kaaden <github@kaaden.net>
2023-07-27 21:55:33 +0200

    Move the CORS configuration to the config package

--- src/main/java/com/example/reduxserverspring/config/CorsConfigurer.java
+++ src/main/java/com/example/reduxserverspring/config/CorsConfigurer.java
@@ -1,4 +1,4 @@
-package com.example.reduxserverspring.counters;
+package com.example.reduxserverspring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
